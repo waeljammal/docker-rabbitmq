ce8fa17c65e094e1e6f4422a4d5fcf564286d9a2
Wael <waeljammal@msn.com>
2016-08-27 08:01:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ if [ ! -f /.rabbitmq_password_set ]; then
 	/set_rabbitmq_password.sh
 fi
 
+if [[ "${RABBIT_ULIMIT}" ]]; then
+    ulimit -n "${RABBIT_ULIMIT}"
+else
+    ulimit -n 64000
+fi
+
 # make rabbit own its own files
 echo "Make RabbitMQ own its own files..."
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
